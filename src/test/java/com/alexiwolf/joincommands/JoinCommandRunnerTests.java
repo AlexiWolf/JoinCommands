eb1be87d1f6543a16d52b6a7a8feec10ebf1782d
AlexiWolf <alexiwolf@pm.me>
2021-09-11 06:10:09 -0500

    Make test fixtures public and static

--- src/test/java/com/alexiwolf/joincommands/JoinCommandRunnerTests.java
+++ src/test/java/com/alexiwolf/joincommands/JoinCommandRunnerTests.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class JoinCommandRunnerTests {
+public class JoinCommandRunnerTests {
 
     JoinCommandRunner joinCommandRunner;
 
@@ -56,19 +56,19 @@ class JoinCommandRunnerTests {
         }
     }
 
-    public OfflinePlayer mockNewPlayer() {
+    public static OfflinePlayer mockNewPlayer() {
         OfflinePlayer offlinePlayer = mock(OfflinePlayer.class);
         when(offlinePlayer.hasPlayedBefore()).thenReturn(false);
         return offlinePlayer;
     }
 
-    public OfflinePlayer mockReturningPlayer() {
+    public static OfflinePlayer mockReturningPlayer() {
         OfflinePlayer offlinePlayer = mock(OfflinePlayer.class);
         when(offlinePlayer.hasPlayedBefore()).thenReturn(true);
         return offlinePlayer;
     }
 
-    public Player mockPlayer(UUID playerUuid) {
+    public static Player mockPlayer(UUID playerUuid) {
         Player player = mock(Player.class);
         when(player.getUniqueId()).thenReturn(playerUuid);
         return player;
