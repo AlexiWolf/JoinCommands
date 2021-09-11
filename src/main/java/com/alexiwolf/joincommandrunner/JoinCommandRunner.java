cef4f7001f0a5584506496f8f43f4230fd997dd3
AlexiWolf <alexiwolf@pm.me>
2021-09-11 05:14:37 -0500

    Stubbed JoinCommandRunner

--- src/main/java/com/alexiwolf/joincommandrunner/JoinCommandRunner.java
+++ src/main/java/com/alexiwolf/joincommandrunner/JoinCommandRunner.java
@@ -1,6 +1,18 @@
 package com.alexiwolf.joincommandrunner;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public abstract class JoinCommandRunner implements Listener {
+
+    public void OnPlayerJoin(PlayerJoinEvent playerJoinEvent) { }
+
+    public boolean isNewPlayer(PlayerJoinEvent playerJoinEvent) {
+        return false;
+    }
+
+    public abstract void runCommandsForNewPlayer(Player player);
+
+    public abstract void runCommandsForReturningPlayer(Player player);
 }
