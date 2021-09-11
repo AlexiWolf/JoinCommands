6ef550dfe506bc113ff4dbb07e5e8a1f3eeee9c5
AlexiWolf <alexiwolf@pm.me>
2021-09-11 05:47:18 -0500

    Implemented isNewPlayer

--- src/main/java/com/alexiwolf/joincommands/JoinCommandRunner.java
+++ src/main/java/com/alexiwolf/joincommands/JoinCommandRunner.java
@@ -1,15 +1,23 @@
 package com.alexiwolf.joincommands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.UUID;
 
 public abstract class JoinCommandRunner implements Listener {
 
     public void OnPlayerJoin(PlayerJoinEvent playerJoinEvent) { }
 
     public boolean isNewPlayer(PlayerJoinEvent playerJoinEvent) {
-        return false;
+        UUID playerUuid = playerJoinEvent
+                .getPlayer()
+                .getUniqueId();
+        OfflinePlayer player = Bukkit.getOfflinePlayer(playerUuid);
+        return !player.hasPlayedBefore();
     }
 
     public abstract void runCommandsForNewPlayer(Player player);
