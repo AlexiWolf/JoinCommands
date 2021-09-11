d2c49c4017f65cb78cc94211a358bddbea975cf2
AlexiWolf <alexiwolf@pm.me>
2021-09-11 05:07:36 -0500

    Remove unused functions

--- src/main/java/com/alexiwolf/joincommandrunner/JoinCommands.java
+++ src/main/java/com/alexiwolf/joincommandrunner/JoinCommands.java
@@ -5,13 +5,5 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class JoinCommands extends JavaPlugin {
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+    public void onEnable() { }
 }
