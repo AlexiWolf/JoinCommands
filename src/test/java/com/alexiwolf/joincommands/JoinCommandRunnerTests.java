6ffad5561018799daf3fb62412da234b72833b55
AlexiWolf <alexiwolf@pm.me>
2021-09-11 05:38:25 -0500

    Test new players are recognized as new players

--- src/test/java/com/alexiwolf/joincommands/JoinCommandRunnerTests.java
+++ src/test/java/com/alexiwolf/joincommands/JoinCommandRunnerTests.java
@@ -1,9 +1,17 @@
 package com.alexiwolf.joincommands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class JoinCommandRunnerTests {
 
@@ -12,6 +20,35 @@ class JoinCommandRunnerTests {
     @BeforeEach
     public void setup() {
          joinCommandRunner = new TestJoinCommandRunner();
+    }
+
+    @Test
+    void shouldBeANewPlayer() {
+        try (MockedStatic<Bukkit> bukkit = mockStatic(Bukkit.class)) {
+            UUID playerUuid = UUID.randomUUID();
+            Player player = mockPlayer(playerUuid);
+            OfflinePlayer offlinePlayer = mockNewPlayer();
+            PlayerJoinEvent playerJoinEvent = new PlayerJoinEvent(player, "Player has joined.");
+            bukkit.when(() -> Bukkit.getOfflinePlayer(playerUuid))
+                    .thenReturn(offlinePlayer);
+
+            assertTrue(
+                    joinCommandRunner.isNewPlayer(playerJoinEvent),
+                    "The player is not new, but they should be"
+            );
+        }
+    }
+
+    private Player mockPlayer(UUID playerUuid) {
+        Player player = mock(Player.class);
+        when(player.getUniqueId()).thenReturn(playerUuid);
+        return player;
+    }
+
+    private OfflinePlayer mockNewPlayer() {
+        OfflinePlayer offlinePlayer = mock(OfflinePlayer.class);
+        when(offlinePlayer.hasPlayedBefore()).thenReturn(false);
+        return offlinePlayer;
     }
 
     // No-op Implementation
