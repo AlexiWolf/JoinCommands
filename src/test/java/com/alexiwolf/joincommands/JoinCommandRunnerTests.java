1302954cbecaf756a8f4555bb372d01ffbe7b2c1
AlexiWolf <alexiwolf@pm.me>
2021-09-11 05:56:18 -0500

    Expose these mocking fixtures publicly

--- src/test/java/com/alexiwolf/joincommands/JoinCommandRunnerTests.java
+++ src/test/java/com/alexiwolf/joincommands/JoinCommandRunnerTests.java
@@ -39,12 +39,6 @@ class JoinCommandRunnerTests {
         }
     }
 
-    private OfflinePlayer mockNewPlayer() {
-        OfflinePlayer offlinePlayer = mock(OfflinePlayer.class);
-        when(offlinePlayer.hasPlayedBefore()).thenReturn(false);
-        return offlinePlayer;
-    }
-
     @Test
     void shouldBeAReturningPlayer() {
         try (MockedStatic<Bukkit> bukkit = mockStatic(Bukkit.class)) {
@@ -62,17 +56,24 @@ class JoinCommandRunnerTests {
         }
     }
 
-    private OfflinePlayer mockReturningPlayer() {
+    public OfflinePlayer mockNewPlayer() {
+        OfflinePlayer offlinePlayer = mock(OfflinePlayer.class);
+        when(offlinePlayer.hasPlayedBefore()).thenReturn(false);
+        return offlinePlayer;
+    }
+
+    public OfflinePlayer mockReturningPlayer() {
         OfflinePlayer offlinePlayer = mock(OfflinePlayer.class);
         when(offlinePlayer.hasPlayedBefore()).thenReturn(true);
         return offlinePlayer;
     }
 
-    private Player mockPlayer(UUID playerUuid) {
+    public Player mockPlayer(UUID playerUuid) {
         Player player = mock(Player.class);
         when(player.getUniqueId()).thenReturn(playerUuid);
         return player;
     }
+
     // No-op Implementation
     private static class TestJoinCommandRunner extends JoinCommandRunner {
         @Override
