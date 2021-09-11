ba456ecdc0c1a65a44ef248dcd68d459519f39cc
AlexiWolf <alexiwolf@pm.me>
2021-09-11 05:46:47 -0500

    Returning players should be recognized

--- src/test/java/com/alexiwolf/joincommands/JoinCommandRunnerTests.java
+++ src/test/java/com/alexiwolf/joincommands/JoinCommandRunnerTests.java
@@ -34,15 +34,9 @@ class JoinCommandRunnerTests {
 
             assertTrue(
                     joinCommandRunner.isNewPlayer(playerJoinEvent),
-                    "The player is not new, but they should be"
+                    "The player is returning, but they should be a new player."
             );
         }
-    }
-
-    private Player mockPlayer(UUID playerUuid) {
-        Player player = mock(Player.class);
-        when(player.getUniqueId()).thenReturn(playerUuid);
-        return player;
     }
 
     private OfflinePlayer mockNewPlayer() {
@@ -51,6 +45,34 @@ class JoinCommandRunnerTests {
         return offlinePlayer;
     }
 
+    @Test
+    void shouldBeAReturningPlayer() {
+        try (MockedStatic<Bukkit> bukkit = mockStatic(Bukkit.class)) {
+            UUID playerUuid = UUID.randomUUID();
+            Player player = mockPlayer(playerUuid);
+            OfflinePlayer offlinePlayer = mockReturningPlayer();
+            PlayerJoinEvent playerJoinEvent = new PlayerJoinEvent(player, "Player has joined.");
+            bukkit.when(() -> Bukkit.getOfflinePlayer(playerUuid))
+                    .thenReturn(offlinePlayer);
+
+            assertFalse(
+                    joinCommandRunner.isNewPlayer(playerJoinEvent),
+                    "The player is new, but they should be a returning player."
+            );
+        }
+    }
+
+    private OfflinePlayer mockReturningPlayer() {
+        OfflinePlayer offlinePlayer = mock(OfflinePlayer.class);
+        when(offlinePlayer.hasPlayedBefore()).thenReturn(true);
+        return offlinePlayer;
+    }
+
+    private Player mockPlayer(UUID playerUuid) {
+        Player player = mock(Player.class);
+        when(player.getUniqueId()).thenReturn(playerUuid);
+        return player;
+    }
     // No-op Implementation
     private static class TestJoinCommandRunner extends JoinCommandRunner {
         @Override
