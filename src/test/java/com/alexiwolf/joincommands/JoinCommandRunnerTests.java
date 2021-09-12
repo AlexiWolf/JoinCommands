6c54881725be9f96772757980e99fba89c848877
AlexiWolf <alexiwolf@pm.me>
2021-09-12 14:32:26 -0500

    Moved the test fixtures to the bottom of the file

--- src/test/java/com/alexiwolf/joincommands/JoinCommandRunnerTests.java
+++ src/test/java/com/alexiwolf/joincommands/JoinCommandRunnerTests.java
@@ -32,7 +32,6 @@ public class JoinCommandRunnerTests {
         joinCommandRunner = new JoinCommandRunner(newPlayerCommands, returningPlayerCommands);
     }
 
-
     @Test
     void shouldBeANewPlayer() {
         try (MockedStatic<Bukkit> bukkit = mockStatic(Bukkit.class)) {
@@ -67,24 +66,6 @@ public class JoinCommandRunnerTests {
         }
     }
 
-    public static OfflinePlayer mockNewPlayer() {
-        OfflinePlayer offlinePlayer = mock(OfflinePlayer.class);
-        when(offlinePlayer.hasPlayedBefore()).thenReturn(false);
-        return offlinePlayer;
-    }
-
-    public static OfflinePlayer mockReturningPlayer() {
-        OfflinePlayer offlinePlayer = mock(OfflinePlayer.class);
-        when(offlinePlayer.hasPlayedBefore()).thenReturn(true);
-        return offlinePlayer;
-    }
-
-    public static Player mockPlayer(UUID playerUuid) {
-        Player player = mock(Player.class);
-        when(player.getUniqueId()).thenReturn(playerUuid);
-        return player;
-    }
-
     @Test
     void shouldRunNewPlayerCommandsForNewPlayers() {
         try (MockedStatic<Bukkit> bukkit = mockStatic(Bukkit.class)) {
@@ -116,5 +97,23 @@ public class JoinCommandRunnerTests {
 
             verify(player, times(1)).performCommand("motd");
         }
+    }
+
+    public static OfflinePlayer mockNewPlayer() {
+        OfflinePlayer offlinePlayer = mock(OfflinePlayer.class);
+        when(offlinePlayer.hasPlayedBefore()).thenReturn(false);
+        return offlinePlayer;
+    }
+
+    public static OfflinePlayer mockReturningPlayer() {
+        OfflinePlayer offlinePlayer = mock(OfflinePlayer.class);
+        when(offlinePlayer.hasPlayedBefore()).thenReturn(true);
+        return offlinePlayer;
+    }
+
+    public static Player mockPlayer(UUID playerUuid) {
+        Player player = mock(Player.class);
+        when(player.getUniqueId()).thenReturn(playerUuid);
+        return player;
     }
 }
