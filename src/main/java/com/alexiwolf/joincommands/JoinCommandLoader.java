4cfe66023c7c7b1b4b8d0c2ddcf2c0926da3ebca
AlexiWolf <alexiwolf@pm.me>
2021-09-12 17:57:41 -0500

    Stubbed getNewPlayerCommands

--- src/main/java/com/alexiwolf/joincommands/JoinCommandLoader.java
+++ src/main/java/com/alexiwolf/joincommands/JoinCommandLoader.java
@@ -1,4 +1,13 @@
 package com.alexiwolf.joincommands;
 
+import com.alexiwolf.joincommands.commands.JoinCommand;
+import org.bukkit.Server;
+import org.bukkit.configuration.Configuration;
+
+import java.util.List;
+
 public class JoinCommandLoader {
+    public static List<JoinCommand> getNewPlayerCommands(Configuration config, Server server) {
+        return null;
+    }
 }
