f1195224be472d756aa5fe473fd7a79d4f9ac1d8
AlexiWolf <alexiwolf@pm.me>
2021-09-12 14:39:29 -0500

    Test the command is run by the player

--- src/test/java/com/alexiwolf/joincommands/JoinCommandTests.java
+++ src/test/java/com/alexiwolf/joincommands/JoinCommandTests.java
@@ -6,12 +6,17 @@ import org.junit.jupiter.api.Test;
 import java.util.UUID;
 
 import static com.alexiwolf.joincommands.JoinCommandRunnerTests.mockPlayer;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class JoinCommandTests {
     @Test
     void shouldRunPlayerCommandsAsThePlayer() {
         Player player = mockPlayer(UUID.randomUUID());
         JoinCommand command = new JoinCommand("motd", RunAs.PLAYER);
+
+        command.runFor(player);
+
+        verify(player, times(1)).performCommand("motd");
     }
 }
