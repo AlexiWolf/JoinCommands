21e02a6c885d46b26e4b495735e57612b4d80450
AlexiWolf <alexiwolf@pm.me>
2021-09-12 17:58:54 -0500

    Implemented command loader for player commands

--- src/main/java/com/alexiwolf/joincommands/JoinCommandLoader.java
+++ src/main/java/com/alexiwolf/joincommands/JoinCommandLoader.java
@@ -1,13 +1,34 @@
 package com.alexiwolf.joincommands;
 
+import com.alexiwolf.joincommands.commands.ConsoleJoinCommand;
 import com.alexiwolf.joincommands.commands.JoinCommand;
+import com.alexiwolf.joincommands.commands.PlayerJoinCommand;
 import org.bukkit.Server;
 import org.bukkit.configuration.Configuration;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class JoinCommandLoader {
     public static List<JoinCommand> getNewPlayerCommands(Configuration config, Server server) {
-        return null;
+        return Objects.requireNonNull(
+                config.getConfigurationSection("new_player_commands"),
+                "The 'new_player_commands' section could not be found."
+        )
+                .getKeys(false)
+                .stream()
+                .map(command -> extract_command(config, server, command))
+                .collect(Collectors.toList());
+    }
+
+    private static JoinCommand extract_command(Configuration config, Server server, String command) {
+        String command_path = "new_player_commands." + command;
+        String command_type = config.getString(command_path + ".run_as");
+        if (command_type.equalsIgnoreCase("console")) {
+            return new PlayerJoinCommand(command);
+        } else {
+            return new ConsoleJoinCommand(command, server);
+        }
     }
 }
