7499d56559a98dbe64cf8c5a7fe0ac7b1b25503a
AlexiWolf <alexiwolf@pm.me>
2021-09-12 14:24:52 -0500

    Defined JoinCommand constructor

---
+++ src/test/java/com/alexiwolf/joincommands/JoinCommandTests.java
@@ -0,0 +1,17 @@
+package com.alexiwolf.joincommands;
+
+import org.bukkit.entity.Player;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static com.alexiwolf.joincommands.JoinCommandRunnerTests.mockPlayer;
+import static org.junit.jupiter.api.Assertions.*;
+
+class JoinCommandTests {
+    @Test
+    void shouldRunPlayerCommandsAsThePlayer() {
+        Player player = mockPlayer(UUID.randomUUID());
+        JoinCommand command = JoinCommand("motd", RunAs.PLAYER);
+    }
+}
