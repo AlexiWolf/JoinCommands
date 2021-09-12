30fbc158d528cf81f4ad04f0a887e59eeb5b458c
AlexiWolf <alexiwolf@pm.me>
2021-09-12 14:51:46 -0500

    Test the command is run as the server console

--- src/test/java/com/alexiwolf/joincommands/JoinCommandTests.java
+++ src/test/java/com/alexiwolf/joincommands/JoinCommandTests.java
@@ -1,13 +1,16 @@
 package com.alexiwolf.joincommands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 import java.util.UUID;
 
 import static com.alexiwolf.joincommands.JoinCommandRunnerTests.mockPlayer;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class JoinCommandTests {
     @Test
@@ -18,5 +21,22 @@ class JoinCommandTests {
         command.runFor(player);
 
         verify(player, times(1)).performCommand("motd");
+    }
+
+    @Test
+    void shouldRunConsoleCommandsAsTheServerConsole() {
+        try (MockedStatic<Bukkit> bukkit = mockStatic(Bukkit.class)) {
+            Server server = mock(Server.class);
+            ConsoleCommandSender consoleCommandSender = mock(ConsoleCommandSender.class);
+            when(server.getConsoleSender()).thenReturn(consoleCommandSender);
+            bukkit.when(Bukkit::getServer).thenReturn(server);
+            Player player = mockPlayer(UUID.randomUUID());
+            JoinCommand command = new JoinCommand("motd", RunAs.SERVER_CONSOLE);
+
+            command.runFor(player);
+
+            verify(server, times(1)).dispatchCommand(consoleCommandSender, "motd");
+        }
+
     }
 }
