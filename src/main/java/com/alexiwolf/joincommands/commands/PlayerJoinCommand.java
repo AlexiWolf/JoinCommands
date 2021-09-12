e418b9e8306043ca8f751a81a3054df9b28bc68b
AlexiWolf <alexiwolf@pm.me>
2021-09-12 15:02:47 -0500

    Created PlayerJoinCommand

---
+++ src/main/java/com/alexiwolf/joincommands/commands/PlayerJoinCommand.java
@@ -0,0 +1,15 @@
+package com.alexiwolf.joincommands.commands;
+
+import org.bukkit.entity.Player;
+
+public class PlayerJoinCommand extends JoinCommand {
+
+    public PlayerJoinCommand(String text) {
+        super(text);
+    }
+
+    @Override
+    public void runFor(Player player) {
+
+    }
+}
