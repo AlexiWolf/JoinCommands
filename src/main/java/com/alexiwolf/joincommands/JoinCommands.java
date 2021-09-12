9cdd0d530a214cd65d15eb8b0ec1148ea562e75b
AlexiWolf <alexiwolf@pm.me>
2021-09-12 18:41:32 -0500

    Added some console feedback on startup

--- src/main/java/com/alexiwolf/joincommands/JoinCommands.java
+++ src/main/java/com/alexiwolf/joincommands/JoinCommands.java
@@ -1,11 +1,15 @@
 package com.alexiwolf.joincommands;
 
+import com.alexiwolf.joincommands.commands.ConsoleJoinCommand;
 import com.alexiwolf.joincommands.commands.JoinCommand;
+import com.alexiwolf.joincommands.commands.PlayerJoinCommand;
+import jdk.internal.joptsimple.internal.Strings;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public final class JoinCommands extends JavaPlugin {
 
@@ -24,8 +28,16 @@ public final class JoinCommands extends JavaPlugin {
     private JoinCommandRunner getJoinCommandRunner(FileConfiguration config) {
         List<JoinCommand> newPlayerCommands = JoinCommandLoader
                 .getNewPlayerCommands(config, Bukkit.getServer());
+        logCommandList(newPlayerCommands, "new player");
         List<JoinCommand> returningPlayerCommands = JoinCommandLoader
                 .getReturningPlayerCommands(config, Bukkit.getServer());
+        logCommandList(returningPlayerCommands, "returning player");
         return new JoinCommandRunner(newPlayerCommands, returningPlayerCommands);
+    }
+
+    private void logCommandList(List<JoinCommand> commands, String commandGroup) {
+        Logger logger = getLogger();
+        logger.info("Loaded " + commands.size() + " " + commandGroup + " commands.");
+        commands.forEach(command -> logger.info(command.toString()));
     }
 }
