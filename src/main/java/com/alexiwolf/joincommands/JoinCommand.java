18231901cbd7a526e5e7bff237e28270e1dd475a
AlexiWolf <alexiwolf@pm.me>
2021-09-12 14:26:22 -0500

    Implemented JoinCommand's constructor

--- src/main/java/com/alexiwolf/joincommands/JoinCommand.java
+++ src/main/java/com/alexiwolf/joincommands/JoinCommand.java
@@ -1,5 +1,11 @@
 package com.alexiwolf.joincommands;
 
 public class JoinCommand {
+    private String command;
+    private RunAs runAs;
 
+    public JoinCommand(String command, RunAs runAs) {
+        this.command = command;
+        this.runAs = runAs;
+    }
 }
