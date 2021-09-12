33a89fd07fcf5e29fea2cf674614f6c5dd370166
AlexiWolf <alexiwolf@pm.me>
2021-09-12 18:06:14 -0500

    Implemented returning player command loader

--- src/main/java/com/alexiwolf/joincommands/JoinCommandLoader.java
+++ src/main/java/com/alexiwolf/joincommands/JoinCommandLoader.java
@@ -19,18 +19,25 @@ public class JoinCommandLoader {
         )
                 .getKeys(false)
                 .stream()
-                .map(command -> extract_command(config, server, command))
+                .map(command -> extract_command("new", config, server, command))
                 .collect(Collectors.toList());
     }
 
     public static List<JoinCommand> getReturningPlayerCommands(YamlConfiguration config, Server server) {
-        return null;
+        return Objects.requireNonNull(
+                config.getConfigurationSection("returning_player_commands"),
+                "The 'returning_player_commands' section could not be found."
+        )
+                .getKeys(false)
+                .stream()
+                .map(command -> extract_command("returning", config, server, command))
+                .collect(Collectors.toList());
     }
 
-    private static JoinCommand extract_command(Configuration config, Server server, String command) {
-        String command_path = "new_player_commands." + command;
-        String command_type = config.getString(command_path + ".run_as");
-        if (command_type.equalsIgnoreCase("console")) {
+    private static JoinCommand extract_command(String command_type, Configuration config, Server server, String command) {
+        String command_path = command_type + "_player_commands." + command;
+        String command_runner = config.getString(command_path + ".run_as");
+        if (command_runner.equalsIgnoreCase("console")) {
             return new PlayerJoinCommand(command);
         } else {
             return new ConsoleJoinCommand(command, server);
