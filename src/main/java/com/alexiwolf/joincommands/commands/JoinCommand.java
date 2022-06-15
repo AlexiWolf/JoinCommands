6c04399f2c63c84e2076e08f0ddb1ae3c45148bf
AlexiWolf <alexiwolf@pm.me>
2022-06-15 03:35:23 -0500

    Added an optional `permission` to the `JoinCommand`

--- src/main/java/com/alexiwolf/joincommands/commands/JoinCommand.java
+++ src/main/java/com/alexiwolf/joincommands/commands/JoinCommand.java
@@ -1,15 +1,24 @@
 package com.alexiwolf.joincommands.commands;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+
+import java.util.Optional;
+
 import org.bukkit.entity.Player;
 
 public abstract class JoinCommand {
     public final String text;
     public final boolean usePlaceholders;
+    public final Optional<String> permission;
 
-    protected JoinCommand(String text, boolean usePlaceholders) {
+    protected JoinCommand(
+            String text, 
+            boolean usePlaceholders,
+            Optional<String> permission
+        ) {
         this.text = text;
         this.usePlaceholders = usePlaceholders;
+        this.permission = permission;
     }
 
     public abstract void runFor(Player player);
